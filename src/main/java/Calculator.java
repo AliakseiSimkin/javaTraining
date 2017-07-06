3a39f6de3b84971bcdde5dd467c80e1b085a8072
asimkin <aliaksei_simkin@epam.com>
2017-07-06 23:06:49 +0300

    Update the Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,5 @@
 /**
- * Created by Aliaksei_Simkin on 6/21/2017.
+ Created by Aliaksei Simkin
  */
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,14 +9,14 @@ import java.io.InputStreamReader;
 public class Calculator {
     public static void main (String[] args) throws IOException {
 
-        BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
-        System.out.println("Type operation. The following characters in round brackets are allowed: (+ - * / )");
-        String operation = reader.readLine();
-
         System.out.println("Type first number:");
-        double number1 = Double.parseDouble(reader.readLine());
+        double number1 = typeNumber();
         System.out.println("Type second number:");
-        double number2 = Double.parseDouble(reader.readLine());
+        double number2 = typeNumber();
+
+        System.out.println("Type operation. The following characters in round brackets are allowed: (+ - * / )");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String operation = reader.readLine();
 
         double answer;
         switch (operation) {
@@ -33,10 +33,33 @@ public class Calculator {
                 System.out.println(answer);
                 break;
             case "/":
+                do {
+                    if (number2 == 0 ) {
+                        System.out.println("Division by 0 is forbidden. Please type value different from 0");
+                        number2 = typeNumber();
+                    }
+                } while (number2 == 0);
+
                 answer = number1 / number2;
                 System.out.println(answer);
                 break;
+            default:
+                System.out.println("Typed character does not match to any accessible symbols. Please type one of the following symbols: + - * / ");
         }
         System.out.println("Thanks for using. Calculator is down");
+    }
+
+    public static double typeNumber () {
+        double number=0;
+        BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+        try {
+            number = Double.parseDouble(reader.readLine());
+        } catch (Exception e) {
+            System.out.println("Incorrect data has been typed. ");
+            //e.printStackTrace();
+            System.out.println("Please try again with correct data");
+            typeNumber();
+        }
+        return number;
     }
 }
