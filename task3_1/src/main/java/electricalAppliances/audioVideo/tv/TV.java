80e25ce6b3895689eed58e7270b609bc30a59f03
AliakseiSimkin <aliaksei_simkin@epam.com>
2017-09-14 13:17:49 +0300

    Add base infrastructure

---
+++ task3_1/src/main/java/electricalAppliances/audioVideo/tv/TV.java
@@ -0,0 +1,85 @@
+package electricalAppliances.audioVideo.tv;
+
+import electricalAppliances.audioVideo.AudioVideo;
+import electricalAppliances.audioVideo.ChangeVolume;
+
+public class TV extends AudioVideo implements ChangeVolume {
+    protected String tvType; //LCD TV, analog TV, etc
+    protected int screenDiagonal; //43, 45, 55, etc (length in inches)
+    protected String screenResolution; //HD, FullHD, UHD, 1920x1080, etc
+    protected String aspectRatio; //4:3, 16:9, etc
+    protected boolean smartTV; //Does TV support smartTV or not?
+    protected String smartTvType; // if smartTV is true - LG webOS  (3.0), AppleTV, etc
+
+    public TV(String company, String model, int releaseDate, int wattsPower, double weight, double length, double width, double thickness,
+              boolean power, String tvType, int screenDiagonal, String screenResolution, String aspectRatio,
+              boolean smartTV, String smartTvType) {
+        super(company, model, releaseDate, wattsPower, weight, length, width, thickness, power);
+
+        this.tvType = tvType;
+        this.screenDiagonal = screenDiagonal;
+        this.screenResolution = screenResolution;
+        this.aspectRatio = aspectRatio;
+        this.smartTV = smartTV;
+
+        if (smartTV == true) {
+            this.smartTvType = smartTvType;
+        } else {
+            this.smartTvType = "This TV has no any Smart TV";
+        }
+    }
+
+    public String getTvType() {
+        return tvType;
+    }
+    public void setTvType(String tvType) {
+        this.tvType = tvType;
+    }
+
+    public int getScreenDiagonal() {
+        return screenDiagonal;
+    }
+    public void setScreenDiagonal(int screenDiagonal) {
+        this.screenDiagonal = screenDiagonal;
+    }
+
+    public String getScreenResolution() {
+        return screenResolution;
+    }
+    public void setScreenResolution(String screenResolution) {
+        this.screenResolution = screenResolution;
+    }
+
+    public String getAspectRatio() {
+        return aspectRatio;
+    }
+    public void setAspectRatio(String aspectRatio) {
+        this.aspectRatio = aspectRatio;
+    }
+
+    public void setSmartTV(boolean smartTV) {
+        this.smartTV = smartTV;
+    }
+
+    public String getSmartTvType() {
+        return smartTvType;
+    }
+    public void setSmartTvType(String smartTvType) {
+        this.smartTvType = smartTvType;
+    }
+
+    @Override
+    public void volumeUp(int volume) {
+        /* some implementation */
+    }
+
+    @Override
+    public void volumeDown(int volume) {
+        /* some implementation */
+    }
+
+    @Override
+    public String toString() {
+        return "This TV is a the property of " + company + " company, model - " + model + ", Release Date - " + releaseDate + ", watts power - " + wattsPower;
+    }
+}
---
+++ task3_1/src/main/java/electricalAppliances/heatingDevices/coolers/frigde/Fridge.java
@@ -0,0 +1,109 @@
+package electricalAppliances.heatingDevices.coolers.frigde;
+
+import electricalAppliances.heatingDevices.ChangeTemperature;
+import electricalAppliances.heatingDevices.coolers.Coolers;
+
+
+public class Fridge extends Coolers implements ChangeTemperature {
+    protected int minFridgeTemperature;
+    protected int maxFridgeTemperature;
+    protected String caseColor;
+    protected int currentFridgeTemperature;
+    protected boolean freezer;
+    protected int minFreezerTemperature;
+    protected int maxFreezerTemperature;
+    protected int currentFreezerTemperature;
+
+    public Fridge(String company, String model, int releaseDate, int wattsPower, double weight, double length, double width, double thickness,
+                  boolean power, boolean heatingType, int minFridgeTemperature, int maxFridgeTemperature, int currentFridgeTemperature, String caseColor,
+                  boolean freezer, int minFreezerTemperature, int maxFreezerTemperature, int currentFreezerTemperature) {
+        super(company, model, releaseDate, wattsPower, weight, length, width, thickness, power, false);
+
+        this.minFridgeTemperature = minFridgeTemperature;
+        this.maxFridgeTemperature = maxFridgeTemperature;
+        this.currentFridgeTemperature = currentFridgeTemperature;
+        this.caseColor = caseColor;
+        this.freezer = freezer;
+
+
+        if (freezer == true) {
+            this.minFreezerTemperature = minFreezerTemperature;
+            this.maxFreezerTemperature = maxFreezerTemperature;
+        }
+        else {
+            this.minFreezerTemperature = 0;
+            this.maxFreezerTemperature = 0;
+        }
+        this.currentFreezerTemperature = currentFreezerTemperature;
+    }
+
+    public int getMinFridgeTemperature() {
+        return minFridgeTemperature;
+    }
+    public void setMinFridgeTemperature(int minFridgeTemperature) {
+        this.minFridgeTemperature = minFridgeTemperature;
+    }
+
+    public int getMaxFridgeTemperature() {
+        return maxFridgeTemperature;
+    }
+    public void setMaxFridgeTemperature(int maxFridgeTemperature) {
+        this.maxFridgeTemperature = maxFridgeTemperature;
+    }
+
+    public String getCaseColor() {
+        return caseColor;
+    }
+    public void setCaseColor(String caseColor) {
+        this.caseColor = caseColor;
+    }
+
+    public boolean getFreezer() {
+        return this.freezer;
+    }
+    public void setFreezer(boolean freezer) {
+        this.freezer = freezer;
+    }
+
+    public int getCurrentFridgeTemperature() {
+        return currentFridgeTemperature;
+    }
+    public void setCurrentFridgeTemperature(int currentFridgeTemperature) {
+        this.currentFridgeTemperature = currentFridgeTemperature;
+    }
+
+    public int getMinFreezerTemperature() {
+        return minFreezerTemperature;
+    }
+    public void setMinFreezerTemperature(int minFreezerTemperature) {
+        this.minFreezerTemperature = minFreezerTemperature;
+    }
+
+    public int getMaxFreezerTemperature() {
+        return maxFreezerTemperature;
+    }
+    public void setMaxFreezerTemperature(int maxFreezerTemperature) {
+        this.maxFreezerTemperature = maxFreezerTemperature;
+    }
+
+    public int getCurrentFreezerTemperature() {
+        return currentFreezerTemperature;
+    }
+    public void setCurrentFreezerTemperature(int currentFreezerTemperature) {
+        this.currentFreezerTemperature = currentFreezerTemperature;
+    }
+
+    @Override
+    public void temperatureUp (int currentTemperature) {
+        /* some implementation */
+    }
+    @Override
+    public void temperatureDown (int currentTemperature) {
+        /* some implementation */
+    }
+
+    @Override
+    public String toString() {
+        return "Fridge company is " + company + ", model - " + model + ", Release Date - " + releaseDate + ", power in watts - " + wattsPower;
+    }
+}
